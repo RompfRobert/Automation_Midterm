2c476f010147cbe7f91fc4fa7440ea51897b7fa2
RompfRobert <rrompf@ibs-b.hu>
2022-04-12 18:59:11 +0200

    delete everything and start over

---
+++ top-artists.py
@@ -0,0 +1,48 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from datetime import date, timedelta
+import pandas as pd
+import csv
+
+today = date.today()
+last_month = date.today()-timedelta(21)
+
+class BillboardSpider(scrapy.Spider):
+    name = "Home_Assignement"
+    
+    def start_requests(self):
+        yield scrapy.Request(f'https://www.billboard.com/charts/artist-100/{today}')
+        
+    def parse(self, response):
+        artists = response.css('.o-chart-results-list-row-container')
+        for artist in artists: 
+            name = artist.css("#title-of-a-story::text").extract()
+            rank = artist.css(".c-label.a-font-primary-bold-l::text").extract()
+            
+            yield {
+                'name': name[0][14:-5],
+                'rank': rank[0][4:-1]
+            } 
+        
+        for i in range(1, 4):
+            yield(scrapy.Request(f'https://www.billboard.com/charts/artist-100/{today - timedelta(i * 7)}'))
+
+process = CrawlerProcess(settings = {
+    'FEED_URI': f'artists({today})-{last_month}().csv',
+    'FEED_FORMAT': 'csv'
+})
+
+process.crawl(BillboardSpider)
+process.start()
+
+def get_top_artists():
+    df = pd.read_csv(f'artists({today})-{last_month}).csv')
+    with open(f'top-artists({today})-{last_month}).csv', 'w', newline='') as file:
+        artists = df.loc[df['rank'] < 6].set_index('name')
+        unsorted_artists = set(artists.index)
+        sorted_artists = list(unsorted_artists)
+        sorted_artists.sort()
+        writer = csv.writer(file)
+        writer.writerow(sorted_artists)
+    
+get_top_artists()
